47fc03338bab9cba6f5ee665b95388596af56080
arfjava <arrfjava@gmail.com>
2020-02-26 20:11:08 +0330

    main completed

---
+++ src/main/java/ir/maktab/arf/quiz/Quiz.java
@@ -0,0 +1,11 @@
+package ir.maktab.arf.quiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Quiz {
+    public static void main(String[] args) {
+        SpringApplication.run(Quiz.class, args);
+    }
+}
