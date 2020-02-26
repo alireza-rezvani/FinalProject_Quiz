f23a7ea3112773a2dac60d3d7dfa60df7fcf2eed
arfjava <arrfjava@gmail.com>
2020-02-26 21:31:31 +0330

    password encoder bean

---
+++ src/main/java/ir/maktab/arf/quiz/security/Config.java
@@ -0,0 +1,15 @@
+package ir.maktab.arf.quiz.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class Config {
+
+    @Bean
+    PasswordEncoder getPasswordEncoder(){
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+}
